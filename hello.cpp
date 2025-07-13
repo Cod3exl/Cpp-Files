16c00d5da6840a4e549158e7267ef6da31bff5af
Cod3exl <75106738+Cod3exl@users.noreply.github.com>
2025-07-13 16:29:56 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello this is my first program!" << endl << "Is this working well?" << endl << "To print the current c++ version, use the __cplusplus macro." << endl << "Current C++ version: " << __cplusplus << endl;
+
+    return 0;
+}
